1174aa016ee1d950d27d1e91bda0e2acf5dbf003
JesusSanchez26 <Jesus.Sanchez26@alu.uclm.es>
2023-04-10 11:22:13 +0200

    Create ryanair.py

---
+++ ryanair.py
@@ -0,0 +1,15 @@
+import http.client
+
+conn = http.client.HTTPSConnection("ryanair.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Key': "INTRODUCIR CLAVE API",
+    'X-RapidAPI-Host': "ryanair.p.rapidapi.com"
+    }
+
+conn.request("GET", "/flights?origin_code=LGW&destination_code=DUB&origin_departure_date=2023-09-28&destination_departure_date=2023-10-28", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
