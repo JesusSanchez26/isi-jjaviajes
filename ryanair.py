0dda7c1a472ba51f3949a20b06bca600eb279118
JesusSanchez26 <Jesus.Sanchez26@alu.uclm.es>
2023-04-12 10:19:57 +0200

    Update ryanair.py

--- ryanair.py
+++ ryanair.py
@@ -1,15 +1,19 @@
 import http.client
 
-conn = http.client.HTTPSConnection("ryanair.p.rapidapi.com")
+class datos_api:
 
-headers = {
-    'X-RapidAPI-Key': "INTRODUCIR CLAVE API",
-    'X-RapidAPI-Host': "ryanair.p.rapidapi.com"
-    }
+    def vuelos():
 
-conn.request("GET", "/flights?origin_code=LGW&destination_code=DUB&origin_departure_date=2023-09-28&destination_departure_date=2023-10-28", headers=headers)
+        conn = http.client.HTTPSConnection("ryanair.p.rapidapi.com")
 
-res = conn.getresponse()
-data = res.read()
+        headers = {
+            'X-RapidAPI-Key': "INTRODUCIR CLAVE API",
+            'X-RapidAPI-Host': "ryanair.p.rapidapi.com"
+        }
 
-print(data.decode("utf-8"))
+        conn.request("GET", "/flights?origin_code=LGW&destination_code=DUB&origin_departure_date=2023-09-28&destination_departure_date=2023-10-28", headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        return data.decode("utf-8")
