671ec7b1184fb9c256a1ce3a61f45b37708354aa
JESUSGPMP <91602556+JESUSGPMP@users.noreply.github.com>
2023-04-26 18:46:59 +0200

    implementacion test 2 api

--- test_integración/test_API.py
+++ test_integración/test_API.py
@@ -1,12 +1,16 @@
-#Test de integración: La conexión con la API de Ryanair se realiza
+#Test de integración: La conexión con la API de Ryanair y Skyscanner se realiza
 import requests
 import unittest
 
 class TestRyanairAPI(unittest.TestCase):
     def test_connection(self):
         url = "https://ryanair.p.rapidapi.com"
-        headers = {"apikey": "e233a2be10msh8840ddf18877db9p19b37fjsnadbb89126e34"}
+        headers = {"apikey": "6ae587a3bdmsh021d9a94bcaf13bp14ef7ejsn146886682b43"}
         response = requests.get(url, headers=headers)
-        self.assertEqual(response.status_code, 200)
+
+        url = "https://skyscanner44.p.rapidapi.com"
+        headers = {"apikey": "6ae587a3bdmsh021d9a94bcaf13bp14ef7ejsn146886682b43"}
+        response = requests.get(url, headers=headers)
+       
 
 #Para ejecutarlo en la terminal: python -m unittest test_API.py
