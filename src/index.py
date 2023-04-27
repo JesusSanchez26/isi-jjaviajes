f284dd550d3c9d4ae3996c31b7ec7bdfb170cd84
JesusSanchez26 <91555863+JesusSanchez26@users.noreply.github.com>
2023-04-27 12:45:08 +0200

    Index.py

--- src/index.py
+++ src/index.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template,request,redirect
 from string import Template
-import mysql
 
 app = Flask(__name__)
 
@@ -8,9 +7,5 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/buscar')
-def about():
-    return render_template('buscar.html')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
